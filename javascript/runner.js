86f0bc204689e026ede3750856eaed6533835c60
Abby <email": null>
2015-09-15 11:22:49 -0400

    add shake animation to loading bar

--- javascript/runner.js
+++ javascript/runner.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   $("button").on("click", function(){
     $("#info").empty();
-    $("#progress-bar").append('loading...')
+    $("#progress-bar").append('loading' + '<div class="animated shake">...</div>')
     $.getJSON('http://www.telize.com/geoip?callback=?', function(json) {
       $("#progress-bar").empty()
       $("#info").append('IP address: ' + json.ip + "<br>");
